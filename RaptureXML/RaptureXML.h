25ed6784028989ae94e10b9b3e166e1831912a72
Evan Maloney <emaloney@gilt.com>
2015-12-22 23:31:11 -0500

    now that we're building as a framework, we need an
    umbrella header

---
+++ RaptureXML/RaptureXML.h
@@ -0,0 +1,27 @@
+//
+//  RaptureXML.h
+//
+//  Created by Evan Maloney on 12/22/15.
+//  Copyright © 2015 Gilt Groupe. All rights reserved.
+//
+
+#ifndef __OBJC__
+
+#error RaptureXML requires Objective-C
+
+#else
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RaptureXML.
+FOUNDATION_EXPORT double RaptureXMLVersionNumber;
+
+//! Project version string for RaptureXML.
+FOUNDATION_EXPORT const unsigned char RaptureXMLVersionString[];
+
+// import the public headers
+
+#import <RaptureXML/RXMLElement.h>
+
+#endif
+
